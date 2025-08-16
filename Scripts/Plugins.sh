a03d393a33a3fc02b79bf442780ab29517cbe5f9
1317825392 <108745034+1317825392@users.noreply.github.com>
2025-08-16 11:44:28 +0800

    Add files via upload

---
+++ Scripts/Plugins.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#Design Theme
+#git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
+#git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
+#sed -i 's/dark/light/g' luci-app-design-config/root/etc/config/design
+#Argon Theme
+#git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
+#git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
+#Linkease
+git clone --depth=1 --single-branch https://github.com/linkease/istore.git
+git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
+git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
+#Open Clash
+git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
+#Pass Wall
+git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji/openwrt-passwall.git ./pw_luci
+git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji/openwrt-passwall-packages.git ./pw_packages
+
+#预置OpenClash内核和GEO数据
+export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
+export CORE_smart=https://github.com/vernesong/OpenClash/raw/core/dev/smart/clash-linux
+export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
+
+export CORE_TYPE=$(echo $OWRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
+export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
+
+export GEO_MMDB=https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
+export GEO_SITE=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
+export GEO_IP=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
+
+cd ./OpenClash/luci-app-openclash/root/etc/openclash
+
+curl -sfL -o ./Country.mmdb $GEO_MMDB
+curl -sfL -o ./GeoSite.dat $GEO_SITE
+curl -sfL -o ./GeoIP.dat $GEO_IP
+
+mkdir ./core && cd ./core
+
+curl -sfL -o ./smart.tar.gz "$CORE_smart"-"$CORE_TYPE"-"$CORE_smart".tar.gz
+gzip -d ./smart.tar.gz && mv ./smart ./clash_smart
+
+curl -sfL -o ./meta.tar.gz "$CORE_MATE"-"$CORE_TYPE".tar.gz
+tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
+
+
+
+chmod +x ./clash* ; rm -rf ./*.gz
